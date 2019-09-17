b811243fdc38ca91fecf14a5d2333246fa55b6dc
yancaoshu <39769198+yancaoshu@users.noreply.github.com>
2019-09-17 22:49:36 +0800

    Create elacon.sh
    用于判断计算的16个ELACON_strainmode$num中哪一个error on C更小。但是没有显示哪一个NUM最小，到时候再弄吧

---
+++ elacon.sh
@@ -0,0 +1,26 @@
+rm alle.txt
+for dir in `cat the_num_of_df_mbj1nospin.txt | awk '{print $1}'`
+do
+echo $dir >>../alle.txt
+cd $dir
+cd Elastic
+rm -r NUM
+mkdir NUM
+for num in  5 10 20 30 40 50 60 70 80 90 100 110 120 130 140 150
+do
+echo "$num" >>NUM/e.txt
+tail -n 3 ELACON_strainmode$num | head -n 1 >>NUM/e.txt
+done
+sed -i 'N;s/\n/ /g' NUM/e.txt
+sed -i 's/=/ /g' NUM/e.txt
+awk '{print $3,$5,$7}' NUM/e.txt >NUM/e
+sort -n NUM/e >NUM/sor.txt
+sed -i '/*/d' NUM/sor.txt
+sed -i '/^$/d' NUM/sor.txt   #这一步一定要
+a=`head -n 1 NUM/sor.txt | awk '{print $1}'`
+b=`grep "$a" NUM/e.txt | awk '{print $1}' `
+#rm ELACON
+cp ELACON_strainmode$b ELACON
+cat NUM/sor.txt >>../../alle.txt
+cd ../../
+done
